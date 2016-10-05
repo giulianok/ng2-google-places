d023a7aeb9c046df56dbc0a81cbfa551f22cd6eb
Giuliano Kranevitter <gkranevitter@sapient.com>
2016-10-05 19:53:22 -0400

    Add and Remove Markers

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -20,11 +20,13 @@ export class AppComponent implements AfterViewInit {
     errorMessage:string;
     loading:boolean = true;
     loadingMessage:string = 'Finding your location...';
-    map:any;
+    map:google.maps.Map;
     defaultLatLong:ILatLong;
     servicePlaces:google.maps.places.PlacesService;
     searchText:string;
     results:google.maps.places.PlaceResult[];
+    infoWindow:google.maps.InfoWindow;
+    markers:google.maps.Marker[] = [];
 
     constructor() {
         console.log('dada');
@@ -37,7 +39,7 @@ export class AppComponent implements AfterViewInit {
     ngAfterViewInit(): void {
         this.getGeolocation()
             .then((data:ILatLong) => {
-                const location = new google.maps.LatLng(data.lat, data.long);
+                const location:google.maps.LatLng = new google.maps.LatLng(data.lat, data.long);
                 this.createMap(location);
                 this.loading = false;
                 this.loadingMessage = null;
@@ -65,7 +67,7 @@ export class AppComponent implements AfterViewInit {
         });
     }
 
-    createMap(location:any):void {
+    createMap(location:google.maps.LatLng):void {
         this.map = new google.maps.Map(document.getElementById('map'), {
             center: location,
             zoom: 15,
@@ -76,46 +78,92 @@ export class AppComponent implements AfterViewInit {
                 position: ControlPosition.RIGHT_CENTER
             }
         });
-
         this.servicePlaces = new google.maps.places.PlacesService(this.map);
+        this.infoWindow = new google.maps.InfoWindow();
 
         // Add User position
-        this.addMarker(location, 'http://maps.gstatic.com/mapfiles/circle.png');
+        const marker = this.addMarker(location, 'http://maps.gstatic.com/mapfiles/circle.png', false);
+        this.addMarkerEvent(marker, `You're here!`);
     }
 
     onSearch():void {
-        this.loading = true;
+        if (!this.searchText) {
 
-        this.findPlaces()
-            .then((results) => {
-                this.loading = false;
-                this.results = results;
-                console.log(results);
-            });
+            this.errorMessage = 'Please enter a word, ex: Pizza';
+
+        } else {
+
+            this.loading = true;
+
+            this.findPlaces()
+                .then((results: google.maps.places.PlaceResult[]) => {
+                    this.loading = false;
+                    this.results = results;
+                    this.showList = false;
+
+                    this.clearMarkers();
+
+                    results.forEach((result: google.maps.places.PlaceResult) => {
+                        const location: google.maps.LatLng = new google.maps.LatLng(result.geometry.location.lat(), result.geometry.location.lng());
+                        const marker = this.addMarker(location, result.icon);
+                        const template = `
+                            <h1 style="font-size: 20px">${result.name}</h1>
+                            <a href="https://www.google.com/maps/place/${result.formatted_address}" target="_blank">${result.formatted_address}</a>
+                        `;
+                        this.addMarkerEvent(marker, template);
+                    });
+                });
+        }
     }
 
-    private addMarker(location:any, icon:string):void {
+    private addMarker(location:google.maps.LatLng, icon:string, save:boolean = true):google.maps.Marker {
         const marker = new google.maps.Marker({
             map: this.map,
             position: location,
             icon: {
                 url: icon,
                 anchor: new google.maps.Point(10, 10),
-                scaledSize: new google.maps.Size(10, 17)
+                scaledSize: new google.maps.Size(19, 32)
             }
         });
+
+        // Saving the marking to be able to change or delete it
+        if (save)
+            this.markers.push(marker);
+
+        return marker;
+    }
+
+    private addMarkerEvent(marker:google.maps.Marker, content:string):void {
+        // return new Promise((resolve) => {
+            google.maps.event.addListener(marker, 'click', () => {
+                this.infoWindow.setContent(content);
+                this.infoWindow.open(this.map, marker);
+            });
+        // });
     }
 
     private findPlaces():Promise<any> {
         return new Promise((resolve) => {
             const request:google.maps.places.TextSearchRequest = {
-                query: this.searchText
+                query: this.searchText,
+                location: this.map.getCenter(),
+                radius: .001
             };
 
             this.servicePlaces.textSearch(request, (results:google.maps.places.PlaceResult[]) => {
+                console.log('Found', results.length);
+                console.log(results);
                 resolve(results);
             });
         });
+    }
+
+    private clearMarkers():void {
+        this.markers.forEach((marker) => {
+            marker.setMap(null);
+        });
+        this.markers = [];
     }
 
     getPriceLevel(level:number):string {
